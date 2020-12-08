a0d93a3582b5e296fd07ed8809ed2c5a18744708
Geir Ingi Sigurðsson <geir.sigurdsson@gmail.com>
2020-12-08 11:06:17 +0100

    inital commit

---
+++ Client/Pages/FetchData.razor.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using EFKBlazorP2.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace EFKBlazorP2.Client.Pages
+{
+    [Authorize]
+    public partial class FetchData
+    {
+        public WeatherForecast[] forecasts;
+        [Inject]
+        public HttpClient Http { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+            }
+            catch (AccessTokenNotAvailableException exception)
+            {
+                exception.Redirect();
+            }
+        }
+    }
+}
